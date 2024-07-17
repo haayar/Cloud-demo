08b911ab5eafd386cb4926c3d3259e9d9ba7b53e
reptile <wangjiajun627@gmail.com>
2024-07-17 13:51:16 +0800

    initialization

---
+++ src/main/java/com/reptile/Main.java
@@ -0,0 +1,13 @@
+package com.reptile;
+
+/**
+ * @Classname ${NAME}
+ * @Description TODO
+ * @Date 2024/7/17 10:22
+ * Created by wangj
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
